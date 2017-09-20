9ab8bee15cabb45309090a10b07f13b8fa2bdf9a
John Peet <jpeet86@gmail.com>
2017-09-20 13:27:21 +0100

    APP: start

--- server/routes/products.js
+++ server/routes/products.js
@@ -3,6 +3,28 @@ import express from 'express';
 let router = express.Router();
 
 router.get('/', (req, res) => {
+  const todayDate = new Date();
+  const deviceType = ["smart-phone", "feature_phone", "browser"];
+  const randomDeviceType = Math.floor(Math.random() * 3);
+
+  const transation = {
+    "userId" : "bob",
+    "transaction" : {
+        "type": "login",
+        "time": todayDate
+    },
+    "location" : {
+        "longitude": -51.51965,
+        "latitude": 51.51965,
+    },
+    "device" : {
+      "type" : deviceType[randomDeviceType],
+      "os" : "Apple",
+      "model" : "iPhone 7"
+    }
+  }
+  console.log(transation)
+
   const products = [
     { "Supplier": "New Co Ltd",
     "Product": "Small wongle",
